2b0dc781629ab99fdbf1a18b63596e47ce3b40d0
TDrake11 <tantqse172039@fpt.edu.vn>
2025-11-06 20:51:55 +0700

    Add migration Fix_Branch_Realations_V2

---
+++ CamRent_Infrastructure/Persistence/Migrations/20251106135125_Fix_Branch_Realations_V2.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CamRent_Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fix_Branch_Realations_V2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_branches_users_manager_id",
+                table: "branches");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "manager_id",
+                table: "branches",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_branches_users_manager_id",
+                table: "branches",
+                column: "manager_id",
+                principalTable: "users",
+                principalColumn: "id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_branches_users_manager_id",
+                table: "branches");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "manager_id",
+                table: "branches",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_branches_users_manager_id",
+                table: "branches",
+                column: "manager_id",
+                principalTable: "users",
+                principalColumn: "id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
