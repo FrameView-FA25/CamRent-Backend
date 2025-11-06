46b0c286f0e5e01d74141549f795c2aa9a1957c4
TDrake11 <tantqse172039@fpt.edu.vn>
2025-11-06 21:32:37 +0700

    AddScoped<IBranchService, BranchService>() in DI

--- CamRent_Application/DependencyInjection.cs
+++ CamRent_Application/DependencyInjection.cs
@@ -13,6 +13,7 @@ namespace CamRent_Application
 		{
 			services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
 
+			services.AddScoped<IBranchService, BranchService>();
 			services.AddScoped<IAccessoryService, AccessoryService>();
 			services.AddScoped<IAuthService, AuthService>();
 			services.AddScoped<ICameraService, CameraService>();
