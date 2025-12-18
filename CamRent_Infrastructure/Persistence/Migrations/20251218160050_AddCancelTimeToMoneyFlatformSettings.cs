7fa0e82bd391058813be22fbdb6c850a3170c60f
Đức Cường Tạ <taduccuong2003@gmail.com>
2025-12-18 23:05:52 +0700

    Sửa thiếu cột cancel_time trong bảng money_flatform_settings (entity có CancelTime nên EF query select m.cancel_time và bị crash

---
+++ CamRent_Infrastructure/Persistence/Migrations/20251218160050_AddCancelTimeToMoneyFlatformSettings.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CamRent_Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCancelTimeToMoneyFlatformSettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "cancel_time",
+                table: "money_flatform_settings",
+                type: "interval",
+                nullable: false,
+                defaultValue: new TimeSpan(0, 0, 0, 0, 0));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "cancel_time",
+                table: "money_flatform_settings");
+        }
+    }
+}
